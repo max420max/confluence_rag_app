7300bd8d72cdfde1e89d095180f2c8779901f1ce
anujwarsaw <109848716+max420max@users.noreply.github.com>
2025-04-08 11:37:43 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,11 @@
+# Constants
+EMB_OPENAI_ADA = "text-embedding-ada-002"
+EMB_SBERT = None # Chroma takes care
+
+LLM = "gpt-3.5-turbo-0125"
+# LLM = "gpt-3.5-turbo"
+# LLM = "gpt-4-turbo-preview"
+
+DB_DIRECTORY = ".chroma_db"
+
+DB_COLLECTION_NAME = "confluence_qa"
